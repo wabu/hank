70f043decacf9b1985980bb35457aa8663b8b36b
kmochi <kmochi@e44590f4-3ce5-462d-a11c-4bcaef5bc860>
2010-04-22 15:17:53 +0000

    moved test classes to src/test
    git-svn-id: https://dev.spline.de/svn/softspiele/trunk@100 e44590f4-3ce5-462d-a11c-4bcaef5bc860

--- src/test/java/renderer/RendererMapTest.java
+++ src/test/java/renderer/RendererMapTest.java
@@ -4,22 +4,22 @@ import static org.junit.Assert.*;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import org.junit.Test;
 
+import renderer.Renderer;
 import renderer.Entity.*;
 import renderer.rendererMap.RendererMap;
 import renderer.rendererMap.RendererMapImpl;
 import renderer.rendererMap.RendererMapModule;
 
 public class RendererMapTest {
-	@Test public void newMapIsEmpty(){
+	@org.junit.Test public void newMapIsEmpty(){
 		Injector inj = Guice.createInjector(new RendererMapModule());
 		RendererMap map = inj.getInstance(RendererMapImpl.class);
 		Renderer<SimpleEntityRenderer> entyrenderer = map.get(null);
 		assertTrue(entyrenderer==null);		
 	}
 	
-	@Test public void putAndGetOne(){
+	@org.junit.Test public void putAndGetOne(){
 		Injector inj = Guice.createInjector(new RendererMapModule());
 		RendererMap map = inj.getInstance(RendererMapImpl.class);
 		SimpleEntity enty = new SimpleEntity(0, new Box(50.0f,50.0f,50,50));
--- src/test/java/renderer/RendererThreadTest.java
+++ src/test/java/renderer/RendererThreadTest.java
@@ -5,13 +5,14 @@ import static org.junit.Assert.*;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import renderer.rendererThread.RendererThread;
+import renderer.rendererThread.RendererThreadImpl;
 import renderer.rendererThread.RendererThreadModul;
 
 
 public class RendererThreadTest {
 	@org.junit.Test public void teststopRunning(){
 		Injector inj = Guice.createInjector(new RendererThreadModul());
-		RendererThread thread = inj.getInstance(RendererThread.class);
+		RendererThread thread = inj.getInstance(RendererThreadImpl.class);
 		Thread t = new Thread(thread);
 		t.start();
 		thread.stopIt();
@@ -21,13 +22,13 @@ public class RendererThreadTest {
 	
 	@org.junit.Test(expected= IllegalArgumentException.class) public void testSetFpsZero(){
 		Injector inj = Guice.createInjector(new RendererThreadModul());
-		RendererThread thread = inj.getInstance(RendererThread.class);
+		RendererThread thread = inj.getInstance(RendererThreadImpl.class);
 		thread.setFramePerSecond(0);
 	}
 	
 	@org.junit.Test public void testConstruktorFpsZero(){
-		Injector inj = Guice.createInjector(new RendererThreadModul());
-		RendererThread thread = inj.getInstance(RendererThread.class);
+		Injector inj = Guice.createInjector(new RendererThreadModul(0,null,null,null));
+		RendererThread thread = inj.getInstance(RendererThreadImpl.class);
 		assertTrue((int)thread.getFramePerSecond()==60);
 	}
 }
