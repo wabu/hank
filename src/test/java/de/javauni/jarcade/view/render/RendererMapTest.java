e49d5b1e95b41fbeab55e132d608e1dc4fccb382
wabu <wabu@e44590f4-3ce5-462d-a11c-4bcaef5bc860>
2010-04-24 16:52:57 +0000

    fixed guice test setup
    git-svn-id: https://dev.spline.de/svn/softspiele/trunk@106 e44590f4-3ce5-462d-a11c-4bcaef5bc860

--- src/test/java/de/javauni/jarcade/view/render/RendererMapTest.java
+++ src/test/java/de/javauni/jarcade/view/render/RendererMapTest.java
@@ -1,36 +1,65 @@
 package de.javauni.jarcade.view.render;
 
+import com.google.inject.AbstractModule;
 import static org.junit.Assert.*;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.name.Names;
 import de.javauni.jarcade.impl.space.SimpleEntity;
 
-import de.javauni.jarcade.view.render.RendererMap;
+import de.javauni.jarcade.impl.space.SpaceImpl;
+import de.javauni.jarcade.model.scene.Scene;
+import de.javauni.jarcade.view.RendererBindingsModule;
 import de.javauni.jarcade.view.impl.RendererMapImpl;
 import de.javauni.jarcade.view.impl.RendererModule;
-import de.javauni.jarcade.view.render.Renderer;
 import de.javauni.jarcade.view.SimpleEntityRenderer;
+import de.javauni.jarcade.view.output.OutputModule;
 import de.javauni.utils.geom.Box;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RendererMapTest {
+
+    private Injector inj;
+
+    @Before
+    public void setUp() {
+        inj = Guice.createInjector(
+                new RendererModule(),
+                new RendererBindingsModule(),
+                new OutputModule(),
+                new AbstractModule() {
+
+                    @Override
+                    protected void configure() {
+                        bind(Executor.class).annotatedWith(Names.named("channel-broadcast-executor")).
+                                toInstance(Executors.newSingleThreadExecutor());
+                        //bind(SimpleEntity.class).toProvider(Providers.of(null));
+                        bind(Scene.class).to(SpaceImpl.class);
+                    }
+                });
+    }
+
     @Test
     public void newMapIsEmpty() {
-        Injector inj = Guice.createInjector(new RendererModule());
         RendererMap map = inj.getInstance(RendererMapImpl.class);
-        Renderer<? super SimpleEntity> entyrenderer = map.get(new SimpleEntity(0, null));
-        assertTrue("renderer returned from empty map is null", entyrenderer == null);
+        Renderer<? super SimpleEntity> entyrenderer =
+                map.get(new SimpleEntity(0, null));
+        assertTrue("renderer returned from empty map is null", entyrenderer
+                == null);
     }
 
     @Test
     public void putAndGetOne() {
-        Injector inj = Guice.createInjector(new RendererModule());
         RendererMap map = inj.getInstance(RendererMapImpl.class);
         SimpleEntity enty = new SimpleEntity(0, new Box(50.0f, 50.0f, 50, 50));
         Renderer<SimpleEntity> entyrend = new SimpleEntityRenderer();
         map.put(enty, entyrend);
         Renderer<? super SimpleEntity> entyren2 = map.get(enty);
-        assertTrue("the renderer put into the map is returend", entyrend == entyren2);
+        assertTrue("the renderer put into the map is returend", entyrend
+                == entyren2);
     }
 }
--- src/test/java/de/javauni/jarcade/view/render/RendererThreadTest.java
+++ src/test/java/de/javauni/jarcade/view/render/RendererThreadTest.java
@@ -1,35 +1,63 @@
 package de.javauni.jarcade.view.render;
 
+import com.google.inject.AbstractModule;
 import static org.junit.Assert.*;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
+import com.google.inject.name.Names;
+import de.javauni.jarcade.impl.space.SpaceImpl;
+import de.javauni.jarcade.model.scene.Scene;
+import de.javauni.jarcade.view.RendererBindingsModule;
 import de.javauni.jarcade.view.impl.RendererModule;
 import de.javauni.jarcade.view.impl.RendererThreadImpl;
+import de.javauni.jarcade.view.output.OutputModule;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import org.junit.Before;
 import org.junit.Test;
 
-
 public class RendererThreadTest {
-        Injector inj;
 
-        @Test public void teststopRunning(){
-                RendererThread thread = inj.getInstance(RendererThreadImpl.class);
-                Thread t = new Thread(thread);
-                t.start();
-                thread.stopIt();
-                Thread.yield();
-                assertFalse("stop thread is not alive", t.isAlive());
-        }
-        
-        @Test(expected= IllegalArgumentException.class) public void testSetFpsZero(){
-                Injector inj = Guice.createInjector(new RendererModule());
-                RendererThread thread = inj.getInstance(RendererThreadImpl.class);
-                thread.setFramePerSecond(0);
-        }
-        
-        @Test public void testConstruktorFpsZero(){
-                Injector inj = Guice.createInjector(new RendererModule());
-                RendererThread thread = inj.getInstance(RendererThreadImpl.class);
-                assertTrue("fps are positive", (int)thread.getFramePerSecond()>=0);
-        }
+    private Injector inj;
+
+    @Before
+    public void setUp() {
+        inj = Guice.createInjector(
+                new RendererModule(),
+                new RendererBindingsModule(),
+                new OutputModule(),
+                new AbstractModule() {
+
+                    @Override
+                    protected void configure() {
+                        bind(Executor.class).annotatedWith(Names.named("channel-broadcast-executor")).
+                                toInstance(Executors.newSingleThreadExecutor());
+                        //bind(SimpleEntity.class).toProvider(Providers.of(null));
+                        bind(Scene.class).to(SpaceImpl.class);
+                    }
+                });
+    }
+
+    @Test
+    public void teststopRunning() {
+        RendererThread thread = inj.getInstance(RendererThreadImpl.class);
+        Thread t = new Thread(thread);
+        t.start();
+        thread.stopIt();
+        Thread.yield();
+        assertFalse("stop thread is not alive", t.isAlive());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSetFpsZero() {
+        RendererThread thread = inj.getInstance(RendererThreadImpl.class);
+        thread.setFramePerSecond(0);
+    }
+
+    @Test
+    public void testConstruktorFpsZero() {
+        RendererThread thread = inj.getInstance(RendererThreadImpl.class);
+        assertTrue("fps are positive", (int) thread.getFramePerSecond() >= 0);
+    }
 }
