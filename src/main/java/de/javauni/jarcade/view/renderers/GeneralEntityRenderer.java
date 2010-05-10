3f9d164a8773375aaee7bd8b68f48e1d044f2f0b
wabu <wabu@e44590f4-3ce5-462d-a11c-4bcaef5bc860>
2010-05-10 00:30:36 +0000

    usinge float for rendiering (new AShape.Float())
    git-svn-id: https://dev.spline.de/svn/softspiele/trunk@122 e44590f4-3ce5-462d-a11c-4bcaef5bc860

--- src/main/java/de/javauni/jarcade/view/renderers/GeneralEntityRenderer.java
+++ src/main/java/de/javauni/jarcade/view/renderers/GeneralEntityRenderer.java
@@ -1,6 +1,7 @@
 package de.javauni.jarcade.view.renderers;
 
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,7 +34,7 @@ public class GeneralEntityRenderer implements Renderer<Entity> {
         gfx.rotate(entity.getShape().rotation());
 
 		gfx.setColor(Color.black);
-        gfx.fillRect((int)-size.x()/2, (int)-size.y()/2, (int)size.x(), (int)size.y());
+        gfx.fill(new Rectangle2D.Float(-size.x()/2f, -size.y()/2f, size.x(), size.y()));
 
         gfx.setTransform(tr);
 	}
