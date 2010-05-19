589067fe485b0525f47a6bab9d89b67c506ab097
kmochi <kmochi@e44590f4-3ce5-462d-a11c-4bcaef5bc860>
2010-05-19 12:16:50 +0000

    git-svn-id: https://dev.spline.de/svn/softspiele/trunk@139 e44590f4-3ce5-462d-a11c-4bcaef5bc860

--- src/main/java/de/javauni/jarcade/control/KeyboardControlMapImpl.java
+++ src/main/java/de/javauni/jarcade/control/KeyboardControlMapImpl.java
@@ -5,16 +5,19 @@ package de.javauni.jarcade.control;
  * @author Michael Kmoch, Simon Lang
  */
 
+import com.google.inject.Singleton;
 import java.util.HashMap;
 
 import de.javauni.jarcade.utils.Pair;
 
+@Singleton
 public class KeyboardControlMapImpl extends HashMap<Integer, Pair<Integer,ControlEvent>> implements KeyboardControlMap {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5262297185696935710L;
+
 
 	
 }
