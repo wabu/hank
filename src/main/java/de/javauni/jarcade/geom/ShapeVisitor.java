f7befc960834e9628757e88f24dbe9b332f24886
kmochi <kmochi@e44590f4-3ce5-462d-a11c-4bcaef5bc860>
2010-05-23 09:16:24 +0000

    circle not cirle
    git-svn-id: https://dev.spline.de/svn/softspiele/trunk@143 e44590f4-3ce5-462d-a11c-4bcaef5bc860

--- src/main/java/de/javauni/jarcade/geom/ShapeVisitor.java
+++ src/main/java/de/javauni/jarcade/geom/ShapeVisitor.java
@@ -3,7 +3,7 @@ package de.javauni.jarcade.geom;
 
 public interface ShapeVisitor<E> {
     E visit(Bound bound);
-    E visit(Circle cirle);
+    E visit(Circle circle);
     E visit(CompositeShape composite);
     E visit(Polygon poly);
     E visit(Rect rect);
