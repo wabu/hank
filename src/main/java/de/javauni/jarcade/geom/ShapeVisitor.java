5748e8b260242dd68298e09c7d9d63c35f18631f
wabu <wabu@e44590f4-3ce5-462d-a11c-4bcaef5bc860>
2010-06-07 12:28:29 +0000

    @nullable for visitor return
    git-svn-id: https://dev.spline.de/svn/softspiele/trunk@164 e44590f4-3ce5-462d-a11c-4bcaef5bc860

--- src/main/java/de/javauni/jarcade/geom/ShapeVisitor.java
+++ src/main/java/de/javauni/jarcade/geom/ShapeVisitor.java
@@ -1,11 +1,12 @@
 package de.javauni.jarcade.geom;
 
+import javax.annotation.Nullable;
 
 public interface ShapeVisitor<E> {
-    E visit(Bound bound);
-    E visit(Circle circle);
-    E visit(CompositeShape composite);
-    E visit(Polygon poly);
-    E visit(Rect rect);
-    E visit(Shape other);
+    @Nullable E visit(Bound bound);
+    @Nullable E visit(Circle circle);
+    @Nullable E visit(CompositeShape composite);
+    @Nullable E visit(Polygon poly);
+    @Nullable E visit(Rect rect);
+    @Nullable E visit(Shape other);
 }
